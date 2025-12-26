55e8b13574b48f5e24102c0d8e50f9b902540f55
vrrneto-afk <vr.rneto@gmail.com>
2025-12-25 23:25:06 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "sitio-corrego-v1";
+
+const URLS = [
+  "/sitio-corrego-do-pinhal/",
+  "/sitio-corrego-do-pinhal/index.html",
+  "/sitio-corrego-do-pinhal/manifest.json",
+  "/sitio-corrego-do-pinhal/logo.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
